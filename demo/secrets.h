c4f514ac2aff54b3d2a63eb569ec34dced0d2c0d
Shayne O'Sullivan <shayne@vin.li>
2016-07-21 17:04:31 -0500

    Create secrets.h

---
+++ demo/secrets.h
@@ -0,0 +1,15 @@
+//
+//  secrets.h
+//  demo
+//
+//  Created by Shayne O'Sullivan on 7/20/16.
+//  Copyright © 2016 Vinli. All rights reserved.
+//
+
+#ifndef secrets_h
+#define secrets_h
+
+#define CLIENT_ID @"9feb7fbe-da7f-44d3-b577-e17333389074"
+#define REDIRECT_URI @"http://www.iosdevcamp.org"
+
+#endif /* secrets_h */
